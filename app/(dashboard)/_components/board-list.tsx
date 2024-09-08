11034bf5d294571260014c21be9306203ca7360c
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-09-08 16:06:18 +0530

    fix: fixes large bugs and ui not working also adds db functionality using convex

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useQuery } from "convex/react";
 
 import { api } from "@/convex/_generated/api";
--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import {
-  UserButton,
-  OrganizationSwitcher,
+import { 
+  UserButton, 
+  OrganizationSwitcher, 
   useOrganization
 } from "@clerk/nextjs";
 
@@ -18,28 +18,28 @@ export const Navbar = () => {
         <SearchInput />
       </div>
       <div className="block lg:hidden flex-1">
-        <OrganizationSwitcher
-          hidePersonal
-          appearance={{
-            elements: {
-              rootBox: {
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                width: "100%",
-                maxWidth: "376px",
-              },
-              organizationSwitcherTrigger: {
-                padding: "6px",
-                width: "100%",
-                borderRadius: "8px",
-                border: "1px solid #E5E7EB",
-                justifyContent: "space-between",
-                backgroundColor: "white",
-              }
+      <OrganizationSwitcher
+        hidePersonal
+        appearance={{
+          elements: {
+            rootBox: {
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+              maxWidth: "376px",
+            },
+            organizationSwitcherTrigger: {
+              padding: "6px",
+              width: "100%",
+              borderRadius: "8px",
+              border: "1px solid #E5E7EB",
+              justifyContent: "space-between",
+              backgroundColor: "white",
             }
-          }}
-        />
+          }
+        }}
+      />
       </div>
       {organization && (
         <InviteButton />
